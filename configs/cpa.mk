8e6f712a9acb7804064f9003a87c9ced1cdb4496
zodex <mimirower45@wp.pl>
2014-05-05 14:11:29 +0200

    Devices come official :)

--- configs/cpa.mk
+++ configs/cpa.mk
@@ -3,3 +3,8 @@ PRODUCT_PACKAGES += \
     Trebuchet \
     OmniSwitch \
     DSPManager
+
+# Official builds
+ifneq ($(filter cpa_huashan cpa_e610 cpa_p700 cpa_honami,$(TARGET_PRODUCT)),)
+ROM_VERSION_TAG := OFFICIAL
+endif
