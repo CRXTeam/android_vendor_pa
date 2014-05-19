9b6db25d31613d33dd93af83ae0b38525fdadc4f
zodex <mimirower45@wp.pl>
2014-05-19 20:20:10 +0200

    ViPER

--- configs/cpa.mk
+++ configs/cpa.mk
@@ -2,3 +2,7 @@
 PRODUCT_PACKAGES += \
     Launcher3 \
     OmniSwitch
+
+# ViPER
+PRODUCT_COPY_FILES += \
+    vendor/cpa/prebuilt/apk/ViPER4Android_FX_A4.x.apk:system/app/ViPER4Android_FX_A4.x.apk
