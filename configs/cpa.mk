57b83dcbf11b8a85de5d74a46acbd7654ddacfc7
zodex <mimirower45@wp.pl>
2014-09-20 21:45:52 +0200

    Nova launcher

--- configs/cpa.mk
+++ configs/cpa.mk
@@ -7,4 +7,5 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     vendor/cpa/prebuilt/apk/ViPER4Android_FX_A4.x.apk:system/app/ViPER4Android_FX_A4.x.apk \
     vendor/cpa/prebuilt/apk/wallpapers.apk:system/app/wallpapers.apk \
+    vendor/cpa/prebuilt/apk/Nova.apk:system/app/Nova.apk \
     vendor/cpa/CHANGELOG.mkdn:system/etc/CHANGELOG.mkdn
