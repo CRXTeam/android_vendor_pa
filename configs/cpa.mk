e6e98dd39b5893932943a2fbf996f698b5894dc3
zodex <mimirower45@wp.pl>
2014-09-20 21:50:48 +0200

    Back to build CPAWallpapers

--- configs/cpa.mk
+++ configs/cpa.mk
@@ -1,11 +1,11 @@
 # CrystalPA
 PRODUCT_PACKAGES += \
     OmniSwitch \
-    CMFileManager
+    CMFileManager \
+    CPAWallpapers
 
-# Prebuild apps
+# ViPER
 PRODUCT_COPY_FILES += \
     vendor/cpa/prebuilt/apk/ViPER4Android_FX_A4.x.apk:system/app/ViPER4Android_FX_A4.x.apk \
-    vendor/cpa/prebuilt/apk/wallpapers.apk:system/app/wallpapers.apk \
     vendor/cpa/prebuilt/apk/Nova.apk:system/app/Nova.apk \
     vendor/cpa/CHANGELOG.mkdn:system/etc/CHANGELOG.mkdn
