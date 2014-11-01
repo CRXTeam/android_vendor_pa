46e33ce518fb5c5c43699c4fec79b4825bc3182c
cr3pt <cr3pt@o2.pl>
2014-11-01 13:05:09 +0100

    Fix bootanimation sizes

--- configs/bootanimation.mk
+++ configs/bootanimation.mk
@@ -13,22 +13,31 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
+#1920x1080
 ifneq ($(filter cpa_honami cpa_bacon,$(TARGET_PRODUCT)),)
     PRODUCT_COPY_FILES += \
         vendor/cpa/prebuilt/bootanimation/1920x1080.zip:system/media/bootanimation.zip
 endif
 
+#1280x720
 ifneq ($(filter cpa_huashan cpa_d605,$(TARGET_PRODUCT)),)
     PRODUCT_COPY_FILES += \
         vendor/cpa/prebuilt/bootanimation/1280x720.zip:system/media/bootanimation.zip
 endif
 
-ifneq ($(filter cpa_codina cpa_janice cpa_d620 cpa_p700 cpa_705 cpa_p760 cpa_eagle cpa_w7,$(TARGET_PRODUCT)),)
+#960x540
+ifneq ($(filter cpa_eagle cpa_d620 cpa_w7 cpa_p760,$(TARGET_PRODUCT)),)
+    PRODUCT_COPY_FILES += \
+        vendor/cpa/prebuilt/bootanimation/960x540.zip:system/media/bootanimation.zip
+endif
+
+#800x480
+ifneq ($(filter cpa_codina cpa_janice cpa_p700 cpa_705,$(TARGET_PRODUCT)),)
     PRODUCT_COPY_FILES += \
         vendor/cpa/prebuilt/bootanimation/800x480.zip:system/media/bootanimation.zip
 endif
 
+#480x320
 ifneq ($(filter cpa_e610,$(TARGET_PRODUCT)),)
     PRODUCT_COPY_FILES += \
         vendor/cpa/prebuilt/bootanimation/480x320.zip:system/media/bootanimation.zip
