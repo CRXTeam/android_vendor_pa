379c5c958e6534b53ca5322b5d3441a782660995
Rammar515 <rammarad515@gmail.com>
2014-12-02 17:59:13 +0100

    Add new changelog and more lollipop update

--- configs/cpa.mk
+++ configs/cpa.mk
@@ -1,12 +1,12 @@
 # CrystalPA
 PRODUCT_PACKAGES += \
-    OmniSwitch \
-    CMFileManager \
+#    OmniSwitch \
+#    CMFileManager \
     CPAWallpapers \
-    PerformanceControl
+#    PerformanceControl
 
 # ViPER
 PRODUCT_COPY_FILES += \
     vendor/cpa/prebuilt/apk/ViPER4Android_FX_A4.x.apk:system/app/ViPER4Android_FX_A4.x.apk \
     vendor/cpa/prebuilt/apk/Nova.apk:system/app/Nova.apk \
-    vendor/cpa/CHANGELOG.mkdn:system/CHANGELOG.txt
+    vendor/cpa/CHANGELOG_lollipop.mkdn:system/CHANGELOG_lollipop.txt
