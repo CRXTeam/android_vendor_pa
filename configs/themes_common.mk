c4ae737ae0390809a66f49f7f30e0086c104afd4
cr3pt <cr3pt@o2.pl>
2015-01-27 21:30:59 +0100

    Themes: Port to CM12 [5/6] and sepolicy: Let drmserver scan themes

--- configs/themes_common.mk
+++ configs/themes_common.mk
@@ -1,8 +1,7 @@
 # Theme engine
-# PRODUCT_PACKAGES += \
-#    ThemeChooser \
-#    ThemesProvider
+PRODUCT_PACKAGES += \
+	ThemeChooser \
+	ThemesProvider
 
-# PRODUCT_COPY_FILES += \
-#    vendor/cpa/configs/permissions/com.tmobile.software.themes.xml:system/etc/permissions/com.tmobile.software.themes.xml \
-#    vendor/cpa/configs/permissions/org.cyanogenmod.theme.xml:system/etc/permissions/org.cyanogenmod.theme.xml
+PRODUCT_COPY_FILES += \
+	vendor/pa/configs/permissions/org.cyanogenmod.theme.xml:system/etc/permissions/org.cyanogenmod.theme.xml
