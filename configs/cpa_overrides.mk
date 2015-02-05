5ea9cc39ed8c555a2d4aa456bccd7dce36f8f47c
zodex <mimirower45@wp.pl>
2015-02-05 19:15:54 +0100

    Add Superuser

--- configs/cpa_overrides.mk
+++ configs/cpa_overrides.mk
@@ -13,9 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# PRODUCT_PACKAGES += \
-    busybox \
-    Lightbulb
+PRODUCT_PACKAGES += \
+    busybox
 
 # CrystalPA
 PRODUCT_PACKAGES += \
