7e717b191004bc3a4c908ca73c18cf3b20cbde5c
zodex <mimirower45@wp.pl>
2015-03-15 09:24:20 +0100

    Cleanup in prebuilt apps

--- configs/cpa.mk
+++ configs/cpa.mk
@@ -1,17 +1,6 @@
 # CrystalPA Apps
 PRODUCT_PACKAGES += \
-#    OmniSwitch \
-#    CMFileManager \
     CPAWallpapers \
-#    PerformanceControl
-
-# ViPER FX
-PRODUCT_COPY_FILES += \
-    vendor/cpa/prebuilt/apk/ViPER4Android_FX_A4.x.apk:system/app/ViPER4Android_FX_A4.x.apk
-
-# Nova Launcher
-#PRODUCT_COPY_FILES += \
-#    vendor/cpa/prebuilt/apk/Nova.apk:system/app/Nova.apk
 
 # Changelog
 PRODUCT_COPY_FILES += \
